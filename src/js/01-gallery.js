a00480ea597f0b92077676de358a5717c48fc82f
Sergey Opanasenko <svopanasenko22@gmail.com>
2022-12-17 14:37:11 +0100

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const galleryContainer = document.querySelector('.gallery');
+console.log(galleryContainer);
+
+//--Создание и рендер разметки
+const markup = galleryItems.reduce(
+  (acc, { preview, original, description }) => {
+    return (
+      acc +
+      `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img loading="lazy" class="gallery__image"
+src="${preview}" alt="${description}" data-source="${original}"
+/>
+  </a>
+  </div>`
+    );
+  },
+  ''
+);
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+//..добавление подписи картинке//
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsPosition: 'bottom',
+  captionsDelay: 250,
+});
+console.log(lightbox);
+
