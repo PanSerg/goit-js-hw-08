8b3775dade4456b2c17c54c6e6c7dd518f10c171
Sergey Opanasenko <svopanasenko22@gmail.com>
2022-12-17 18:28:15 +0100

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    console.log(data);
+}
+player.on('play', onPlay);
+
+
+iframe.addEventListener('timeupdate', (evt) => {
+    console.log(evt.target);
+});
+
+console.log(localStorage);
+localStorage.setItem('videoplayer-current-time', 'qweqwe');
+
+player.getCurrentTime(9.32).then(function (seconds) {
+    // seconds = the current playback position
+  })
+  .catch(function (error) {
+    // an error occurred
+  });
+
+  // player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+
 
