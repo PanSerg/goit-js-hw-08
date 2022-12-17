a589aee71ec340f5e53ea96d61e45c1d4131ef28
Sergey Opanasenko <svopanasenko22@gmail.com>
2022-12-17 14:24:36 +0100

    task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,5 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-hello = 'hello world';
-console.log(hello);
+
 console.log(galleryItems);
