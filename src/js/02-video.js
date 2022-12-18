05c6d5d5b8a786e8626bbefb0cda9b137e6107f0
Sergey Opanasenko <svopanasenko22@gmail.com>
2022-12-19 00:39:43 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,14 @@
 import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
-
-const onPlay = function (data) {
-    console.log(data);
-}
-player.on('play', onPlay);
-
-
-iframe.addEventListener('timeupdate', (evt) => {
-    console.log(evt.target);
-});
-
-console.log(localStorage);
-localStorage.setItem('videoplayer-current-time', 'qweqwe');
-
-player.getCurrentTime(9.32).then(function (seconds) {
-    // seconds = the current playback position
-  })
-  .catch(function (error) {
-    // an error occurred
-  });
-
-  // player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
-
-
-
+const PLAYBACK_TIME = 'videoplayer-current-time';
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem(PLAYBACK_TIME, seconds);
+  }, 1000)
+);
+player.setCurrentTime(localStorage.getItem(PLAYBACK_TIME) || 0);
