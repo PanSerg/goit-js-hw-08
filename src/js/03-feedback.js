07c41418503a2a2d769bc62feb502b7cbe436810
Sergey Opanasenko <svopanasenko22@gmail.com>
2022-12-19 01:01:57 +0100

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form, textarea');
 
+form.addEventListener('submit', onFormSubmit);
+textarea.addEventListener('input', onTextareaInput);
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log('Отправить форму');
+  evt.currentTarget.reset();
+};
+
+function onTextareaInput(evt) {
+  const message = evt.currentTarget.value;
+  localStorage.setItem('feadback-form-state', message);
+};
+
+function populateTextarea() {
+  const saveMessage = localStorage.getItem('feadback-form-state');
+
+  if (saveMessage) {
+    console.log(saveMessage);
+    textarea.value = saveMessage;
+  }
+};
