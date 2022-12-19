fb188823cf967c4f95a8bb74cfc43674fe8bf29a
Sergey Opanasenko <svopanasenko22@gmail.com>
2022-12-19 21:58:20 +0100

    task-3.1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,32 @@
+import throttle from "lodash.throttle";
+
 const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('.feedback-form, textarea');
+const textarea = document.querySelector('feedback-form, textarea');
+
+const STORAGE_KEY = 'feadback-form-state';
+const formData = {};
 
 form.addEventListener('submit', onFormSubmit);
-textarea.addEventListener('input', onTextareaInput);
+textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+// вывод данных объекта в localestorage................//
+form.addEventListener('input', e => {
+  // console.log(e.target.name);
+  // console.log(e.target.value);
+
+  formData[e.target.name] = e.target.value;
+  console.log('before', formData);
+
+  const jsonValue = JSON.stringify(formData);
+  console.log('after', jsonValue);
+  console.log('parse', JSON.parse(jsonValue));
+});
+
+localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));
+
+const response = localStorage.getItem('formData');
+console.log(JSON.parse(response));
+//.....................................................//
 
 populateTextarea();
 
@@ -11,18 +35,26 @@ function onFormSubmit(evt) {
 
   console.log('Отправить форму');
   evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 };
 
 function onTextareaInput(evt) {
-  const message = evt.currentTarget.value;
-  localStorage.setItem('feadback-form-state', message);
+  const message = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, message);
+  console.log(message);
 };
 
 function populateTextarea() {
-  const saveMessage = localStorage.getItem('feadback-form-state');
+  const saveMessage = localStorage.getItem(STORAGE_KEY);
 
   if (saveMessage) {
     console.log(saveMessage);
-    textarea.value = saveMessage;
+    textarea.value = saveMessage.name;
+
   }
 };
+
+
+
+
+
